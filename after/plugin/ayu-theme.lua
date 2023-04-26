77c37553d4fdfa13bbcdaee4421ef09a9d77177e
ThomasL <ThomasL81@users.noreply.github.com>
2023-04-26 19:12:22 +0200

    fixed a gitignore issue skipping the after folder

---
+++ after/plugin/ayu-theme.lua
@@ -0,0 +1,7 @@
+local ayu = require('ayu')
+ayu.setup({
+    mirage = true, -- Set to `true` to use `mirage` variant instead of `dark` for dark background.
+    overrides = {}, -- A dictionary of group names, each associated with a dictionary of parameters (`bg`, `fg`, `sp` and `style`) and colors in hex.
+})
+ayu.colorscheme()
+
