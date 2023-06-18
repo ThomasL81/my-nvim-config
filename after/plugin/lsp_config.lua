4e0a6497ae832c0766317ea33678b72546eac9fa
ThomasL <ThomasL81@users.noreply.github.com>
2023-06-18 13:10:08 +0200

    added more diagnostic shortcuts to lsp-config

--- after/plugin/lsp_config.lua
+++ after/plugin/lsp_config.lua
@@ -23,8 +23,9 @@ lspconfig.sap_cds.setup {}
 -- Global mappings.
 -- See `:help vim.diagnostic.*` for documentation on any of the below functions
 vim.keymap.set('n', '<Leader>e', vim.diagnostic.open_float)
-vim.keymap.set('n', '[d', vim.diagnostic.goto_prev)
-vim.keymap.set('n', ']d', vim.diagnostic.goto_next)
+vim.keymap.set('n', '<Leader>dp', vim.diagnostic.goto_prev)
+vim.keymap.set('n', '<Leader>dn', vim.diagnostic.goto_next)
+vim.keymap.set('n', '<Leader>dl', "<cmd>Telescope diagnostics<cr>")
 vim.keymap.set('n', '<Leader>q', vim.diagnostic.setloclist)
 
 -- Use LspAttach autocommand to only map the following keys
@@ -56,6 +57,7 @@ vim.api.nvim_create_autocmd('LspAttach', {
     local opts = { buffer = ev.buf }
     vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, opts)
     vim.keymap.set('n', 'gd', vim.lsp.buf.definition, opts)
+    vim.keymap.set('n', 'gt', vim.lsp.buf.type_definition, opts)
     vim.keymap.set('n', 'K', vim.lsp.buf.hover, opts)
     vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, opts)
     vim.keymap.set('n', '<C-k>', vim.lsp.buf.signature_help, opts)
