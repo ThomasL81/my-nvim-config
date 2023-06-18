dd97687ef07e00f7b082f4e43cfabad29c429bc7
ThomasL <ThomasL81@users.noreply.github.com>
2023-06-18 13:08:41 +0200

    changed vim.o to vim.opt

--- lua/thomasl/map.lua
+++ lua/thomasl/map.lua
@@ -1,38 +1,38 @@
-vim.o.termguicolors=true
+vim.opt.termguicolors=true
 vim.g.mapleader=' '
 
-vim.o.number=true
-vim.o.relativenumber=true
+vim.opt.number=true
+vim.opt.relativenumber=true
 
-vim.o.background="dark"
+vim.opt.background="dark"
 
-vim.o.autoindent=false
-vim.o.smartindent=false
-vim.o.cindent=true
-vim.o.cinwords="if","else","while","do","for"
+vim.opt.autoindent=false
+vim.opt.smartindent=false
+vim.opt.cindent=true
+vim.opt.cinwords="if","else","while","do","for"
 
-vim.o.clipboard="unnamedplus"
+vim.opt.clipboard="unnamedplus"
 
-vim.o.expandtab=true
-vim.o.smarttab=true
-vim.o.shiftwidth=4
-vim.o.tabstop=8
-vim.o.softtabstop=-1                    -- follow shiftwidth setting
-vim.o.wrap=false
+vim.opt.expandtab=true
+vim.opt.smarttab=true
+vim.opt.shiftwidth=4
+vim.opt.tabstop=8
+vim.opt.softtabstop=-1                    -- follow shiftwidth setting
+vim.opt.wrap=false
 
-vim.o.hlsearch=false
-vim.o.ignorecase=true
-vim.o.incsearch=true
+vim.opt.hlsearch=false
+vim.opt.ignorecase=true
+vim.opt.incsearch=true
 
-vim.o.syntax="ON"
-vim.o.termguicolors=true
---vim.o.cursorcolumn=true
---vim.o.colorcolumn="80"
+vim.opt.syntax="ON"
+vim.opt.termguicolors=true
+--vim.opt.cursorcolumn=true
+--vim.opt.colorcolumn="80"
 
-vim.o.scrolloff = 10
-vim.o.signcolumn="yes"
+vim.opt.scrolloff = 10
+vim.opt.signcolumn="yes"
 
-vim.o.updatetime=50
+vim.opt.updatetime=50
 
 vim.g.netrw_fastbrowse=0                -- close the netrw buffer when a file is open
 
@@ -41,7 +41,7 @@ vim.opt.backup = false
 vim.opt.undodir = vim.fn.stdpath('data') .. "/undodir"
 vim.opt.undofile = true
 
-vim.keymap.set("n", "<leader>pv", function() vim.cmd([[Explore]]) end)
+vim.keymap.set("n", "<leader>pv", "<cmd>Explore<cr>") --function() vim.cmd([[Explore]]) end)
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv") 
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv") 
 vim.keymap.set("x", "<leader>p", "\"_dP") 
